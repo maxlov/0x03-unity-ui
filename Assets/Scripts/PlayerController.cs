d97c5be2e70440c5dbce2aac90124bd46b5ccfbe
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-08-04 18:33:52 -0400

    add player input, camera code, coin prefab

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] 
+    InputActionAsset playerControls;
+    InputAction movement;
+
+    public Rigidbody rb;
+
+    [System.NonSerialized]
+    public Vector3 direction;
+    public float speed;
+
+    private void Start()
+    {
+        var gameplayActionMap = playerControls.FindActionMap("Player");
+
+        movement = gameplayActionMap.FindAction("Move");
+
+        movement.performed += OnMovementChanged;
+        movement.canceled += OnMovementChanged;
+        movement.Enable();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(direction * speed);
+    }
+
+    private void OnMovementChanged(InputAction.CallbackContext context)
+    {
+        Vector2 inputDirection = context.ReadValue<Vector2>();
+        inputDirection = Vector2.ClampMagnitude(inputDirection, 1f);
+
+        direction = new Vector3(inputDirection.x, 0f, inputDirection.y);
+    }
+}
