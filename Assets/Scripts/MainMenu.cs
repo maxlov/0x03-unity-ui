08c8d72fd97d7e8fd150dd9cec01f0ccb28f4f66
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-08-12 15:42:41 -0400

    [Create] Color blindness option

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
+    private Color originalTrapColor;
+    private Color originalGoalColor;
+
+    private void Start()
+    {
+        originalTrapColor = trapMat.color;
+        originalGoalColor = goalMat.color;
+    }
+
     public void PlayMaze()
     {
         SceneManager.LoadScene(1);
@@ -14,5 +28,25 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quit Game");
         Application.Quit();
+    }
+
+    public void settingColorBlindToggle()
+    {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = originalTrapColor;
+            goalMat.color = originalGoalColor;
+        }
+    }
+
+    public void OnDestroy()
+    {
+        trapMat.color = originalTrapColor;
+        goalMat.color = originalGoalColor;
     }
 }
